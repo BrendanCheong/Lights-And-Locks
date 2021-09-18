523ab720dead51a4d80b688b57f37b3fac907489
BrendanCheong <brendancej82@gmail.com>
2021-09-18 20:13:42 +0800

    added database secrets into a yaml file, remember to include secrets in python file on final submission

--- Backend Server/db_config.py
+++ Backend Server/db_config.py
@@ -1,14 +1,16 @@
+import yaml
 from app import app
 from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt
 
+db = yaml.load(open('db_secrets.yaml'), Loader=yaml.FullLoader)
 mysql = MySQL(app)
 bcrypt = Bcrypt(app)
 # connect to MySQL database hosted on AWS free tier
-app.config['MYSQL_USER'] = "admin"
-app.config['MYSQL_PASSWORD'] = "BT2102group16"
-app.config['MYSQL_DB'] = "bt2102-oshes"
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
 app.config['MYSQL_PORT'] = 3306
-app.config['MYSQL_HOST'] = "bt2102-oshes.c1j7ubdkf9mu.ap-southeast-1.rds.amazonaws.com"
+app.config['MYSQL_HOST'] = db['mysql_host']
 
 mysql.init_app(app)
